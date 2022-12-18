890e3b778547b3809179defd5f4e467287f248b1
Przemyslaw Bobkowski <przemek.bobkowski@icloud.com>
2022-12-18 00:07:49 -0500

    Reorganizing folders and files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import { ColorModeContext, useMode } from './theme'
 import { CssBaseline, ThemeProvider } from '@mui/material'
+import { Routes, Route } from 'react-router-dom'
 
-import Topbar from './scenes/global/Topbar'
+import Topbar from './scenes/nav/Topbar'
+import Sidebar from './scenes/nav/Sidebar'
+import Dashboard from './scenes/dashboard'
+import Team from './scenes/team'
+import Invoices from './scenes/invoices'
+import Contacts from './scenes/contacts'
+import Bar from './scenes/bar'
+import Form from './scenes/form'
+import Line from './scenes/line'
+import Pie from './scenes/pie'
+import FAQ from './scenes/faq'
+import Geography from './scenes/geography'
+import Calendar from './scenes/calendar/calendar'
+
 
 export default function App() {
   const [theme, colorMode] = useMode()
@@ -11,8 +25,23 @@ export default function App() {
       <ThemeProvider theme={ theme }>
         <CssBaseline />
           <div className="App">
+            <Sidebar />
+            
             <main className='content'>
               <Topbar />
+              <Routes>
+                <Route path='/' element={ <Dashboard /> } />
+                <Route path='/team' element={ <Team /> } />
+                <Route path='/contacts' element={ <Contacts /> } />
+                <Route path='/invoices' element={ <Invoices /> } />
+                <Route path='/form' element={ <Form /> } />
+                <Route path='/bar' element={ <Bar /> } />
+                <Route path='/pie' element={ <Pie /> } />
+                <Route path='/line' element={ <Line /> } />
+                <Route path='/faq' element={ <FAQ /> } />
+                <Route path='/geography' element={ <Geography /> } />
+                <Route path='/calendar' element={ <Calendar /> } />
+              </Routes>
             </main>
           </div>
       </ThemeProvider>
