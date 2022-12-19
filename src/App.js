6ee7be90c6a3e3312a76058871f359d99474ec74
Przemyslaw Bobkowski <przemek.bobkowski@icloud.com>
2022-12-18 20:22:16 -0500

    Finished Sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { ColorModeContext, useMode } from './theme'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
 
 import Topbar from './scenes/nav/Topbar'
 import Sidebar from './scenes/nav/Sidebar'
@@ -19,16 +20,17 @@ import Calendar from './scenes/Calendar'
 
 export default function App() {
   const [theme, colorMode] = useMode()
+  const [isSidebar, setIsSidebar] = useState(true)
 
   return (
     <ColorModeContext.Provider value={ colorMode }>
       <ThemeProvider theme={ theme }>
         <CssBaseline />
-          <div className="App">
-            <Sidebar />
+          <div className="app">
+            <Sidebar isSidebar={ isSidebar } />
             
             <main className='content'>
-              <Topbar />
+              <Topbar setIsSidebar={ setIsSidebar } />
               <Routes>
                 <Route path='/' element={ <Dashboard /> } />
                 <Route path='/team' element={ <Team /> } />
